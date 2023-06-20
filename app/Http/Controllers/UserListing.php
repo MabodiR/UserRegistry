0a5613224aa5c57d289882c152fe3fe738d557ab
Mabodi Rofhiwa <rofhiwa.mabodi@gmail.com>
2023-06-20 23:44:48 +0200

    Updated UserListing Controller with four methods, Index, Update, Show and destroy

--- app/Http/Controllers/UserListing.php
+++ app/Http/Controllers/UserListing.php
@@ -3,8 +3,63 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UserListing extends Controller
 {
+    /**
+     * Display a listing of the users.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return view('user-listings.index', compact('users'));
+    }
 
+    /**
+     * Display the specified user.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        return view('user-listings.show', compact('user'));
+    }
+
+    /**
+     * Update the specified user in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $user->name = $request->input('name');
+        $user->surname = $request->input('surname');
+        $user->email = $request->input('email');
+        $user->position = $request->input('position');
+        $user->save();
+
+        return redirect()->route('user-listings.show', $user->id)->with('success', 'User updated successfully');
+    }
+
+    /**
+     * Remove the specified user from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return redirect()->route('user-listings.index')->with('success', 'User deleted successfully');
+    }
 }
