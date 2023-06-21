dee4ca98958a33c29ef1e75d4a237c1176a7eeb7
Mabodi Rofhiwa <rofhiwa.mabodi@gmail.com>
2023-06-21 17:55:05 +0200

    Added userFactory

---
+++ database/factories/ListingFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\Listing::class, function (Faker $faker) {
+
+    return [
+        'name' => $this->faker->unique()->firstName,
+        'surname' => $this->faker->unique()->lastName,
+        'email' => $this->faker->unique()->safeEmail,
+        'position' => $this->faker->jobTitle,
+        'password' => bcrypt(Str::random(10)), // Use bcrypt to hash the password
+        'remember_token' => Str::random(10),
+    ];
+});
